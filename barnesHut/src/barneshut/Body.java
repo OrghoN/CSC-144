76d7dac5a0efb950863bbc32fdfd7cc088acf1b2
Orgho Neogi <anoronyo@gmail.com>
2016-03-20 13:40:48 -0500

    Added Instance Variables

--- barnesHut/src/barneshut/Body.java
+++ barnesHut/src/barneshut/Body.java
@@ -5,10 +5,22 @@
  */
 package barneshut;
 
+import java.awt.Color;
+
 /**
+ * Class defining each body in the universe
  *
  * @author orgho
  */
 public class Body {
+
+    private static final double G = 6.673e-11; //gravitational constant
+    private static final double solarMass = 1.98892e30; //standard unit equal to mass of sun
+
+    private double[] position = new double[2];
+    private double[] velocity = new double[2];
+    private double[] force = new double[2];
+    private double mass;
+    private Color color;
 
 }
