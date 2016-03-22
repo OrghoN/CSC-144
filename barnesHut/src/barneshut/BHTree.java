41021dfaa2d964b116a1d79a0a32154f3887e941
Orgho Neogi <anoronyo@gmail.com>
2016-03-21 23:42:26 -0500

    finished methods for BHTree

--- barnesHut/src/barneshut/BHTree.java
+++ barnesHut/src/barneshut/BHTree.java
@@ -35,17 +35,112 @@ public class BHTree {
     public void insert(Body b) {
         if (this.body == null) {
             this.body = b;
+        } else if (this.isExternal(this) == false) {
+            this.body = b.add(this.body, b);
+
+            Quadrant northWest = this.quad.NW();
+            if (b.in(northWest)) {
+                if (this.NW == null) {
+                    this.NW = new BHTree(northWest);
+                }
+                NW.insert(b);
+            } else {
+                Quadrant northEast = this.quad.NE();
+                if (b.in(northEast)) {
+                    if (this.NE == null) {
+                        this.NE = new BHTree(northEast);
+                    }
+                    NE.insert(b);
+                } else {
+                    Quadrant southEast = this.quad.SE();
+                    if (b.in(southEast)) {
+                        if (this.SE == null) {
+                            this.SE = new BHTree(southEast);
+                        }
+                        SE.insert(b);
+                    } else {
+                        Quadrant southWest = this.quad.SW();
+                        if (b.in(southWest)) {
+                            if (this.SW == null) {
+                                this.SW = new BHTree(southWest);
+                            }
+                            SW.insert(b);
+                        }
+                    }
+                }
+            }
+
+        } else if (this.isExternal(this)) {
+            Body c = this.body;
+
+            Quadrant northWest = this.quad.NW();
+            if (c.in(northWest)) {
+                if (this.NW == null) {
+                    this.NW = new BHTree(northWest);
+                }
+                NW.insert(c);
+            } else {
+                Quadrant northEast = this.quad.NE();
+                if (c.in(northEast)) {
+                    if (this.NE == null) {
+                        this.NE = new BHTree(northEast);
+                    }
+                    NE.insert(c);
+                } else {
+                    Quadrant southWest = this.quad.SW();
+                    if (c.in(southWest)) {
+                        if (this.SW == null) {
+                            this.SW = new BHTree(southWest);
+                        }
+                        SW.insert(c);
+                    } else {
+                        Quadrant southEast = this.quad.SE();
+                        if (c.in(southEast)) {
+                            if (this.SE == null) {
+                                this.SE = new BHTree(southEast);
+                            }
+                            SE.insert(c);
+                        }
+                    }
+                }
+            }
+
+            this.insert(b);
         }
 
     }
 
     public void updateForce(Body b) {
+        if (this.isExternal(this)) {
+            if (this.body != b) {
+                b.addForce(this.body);
+            }
+        } else if (this.quad.getLength() / this.body.distanceTo(b) < 2) {
+            b.addForce(this.body);
+        } else {
+            if (this.NW != null) {
+                this.NW.updateForce(b);
+            }
+            if (this.NE != null) {
+                this.NE.updateForce(b);
+            }
+            if (this.SW != null) {
+                this.SW.updateForce(b);
+            }
+            if (this.SE != null) {
+                this.SE.updateForce(b);
+            }
+        }
 
     }
 
     @Override
     public String toString() {
-        return "";
+        if (NE != null || NW != null || SW != null || SE != null) {
+            return "*" + this.body + "\n" + this.NW + this.NE + this.SW + this.SE;
+        } else {
+            return " " + this.body + "\n";
+        }
     }
 
     /**
