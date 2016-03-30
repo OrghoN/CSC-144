a08a47ddf12e51248e9ce8a0cbce0a4bae2e36fb
Orgho Neogi <anoronyo@gmail.com>
2016-03-30 05:53:53 -0500

    stripped links from wikipedia

--- WikipediaPathFinder/src/main/java/wikipediapathfinder/Query.java
+++ WikipediaPathFinder/src/main/java/wikipediapathfinder/Query.java
@@ -5,8 +5,12 @@
  */
 package wikipediapathfinder;
 
-import net.sourceforge.jwbf.core.contentRep.Article;
-import net.sourceforge.jwbf.mediawiki.bots.MediaWikiBot;
+import net.sourceforge.jwbf.core.actions.ContentProcessableBuilder;
+import net.sourceforge.jwbf.core.actions.Get;
+import net.sourceforge.jwbf.core.actions.HttpActionClient;
+import net.sourceforge.jwbf.core.actions.ResponseHandler;
+import net.sourceforge.jwbf.mediawiki.ApiRequestBuilder;
+import org.apache.http.impl.client.HttpClientBuilder;
 
 /**
  *
@@ -15,9 +19,37 @@ import net.sourceforge.jwbf.mediawiki.bots.MediaWikiBot;
 public class Query {
 
     public static void main(String[] args) {
-        MediaWikiBot wikiBot = new MediaWikiBot("https://en.wikipedia.org/w/");
-        Article article = wikiBot.getArticle("42");
-        System.out.println(article.getText());
+//        MediaWikiBot wikiBot = new MediaWikiBot("https://en.wikipedia.org/w/");
+//        Article article = wikiBot.getArticle("42");
+//        System.out.println(article.getText());
+
+        HttpActionClient testee = HttpActionClient.builder()
+                .withClient(HttpClientBuilder.create().build())
+                .withUrl("https://www.wikipedia.org/w/")
+                .build();
+        Get search = new ApiRequestBuilder()
+                .formatJson()
+                .action("query")
+                .param("list", "search")
+                .param("srsearch", "wikipedia")
+                .param("limit", "1")
+                .param("maxlax", "-1")
+                .buildGet();
+
+        Get parse = new ApiRequestBuilder()
+                .formatJson()
+                .action("parse")
+                .param("page", "San_Francisco")
+                .param("prop", "links")
+                .buildGet();
+
+        ResponseHandler<String> getResponse = ContentProcessableBuilder.create(testee).withActions(search).build();
+        ResponseHandler<String> getParse = ContentProcessableBuilder.create(testee).withActions(parse).build();
+
+//        System.out.println(testee.performAction(getResponse));
+        testee.performAction(getResponse);
+        System.out.println(getParse.get());
+
     }
 
 }
