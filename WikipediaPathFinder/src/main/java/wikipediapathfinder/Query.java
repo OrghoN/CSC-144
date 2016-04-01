795ee08ffe92cb27bd564e294ea2ba25617b4c48
Orgho Neogi <anoronyo@gmail.com>
2016-04-01 14:09:08 -0500

    got rid of edge labels and made graph visualization work with wikipedia appi

--- WikipediaPathFinder/src/main/java/wikipediapathfinder/Query.java
+++ WikipediaPathFinder/src/main/java/wikipediapathfinder/Query.java
@@ -5,30 +5,52 @@
  */
 package wikipediapathfinder;
 
+import com.jgraph.layout.JGraphFacade;
+import com.jgraph.layout.JGraphLayout;
+import com.jgraph.layout.organic.JGraphOrganicLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Rectangle;
 import java.io.FileNotFoundException;
-import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import javax.swing.JApplet;
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
 import net.sourceforge.jwbf.core.actions.ContentProcessableBuilder;
 import net.sourceforge.jwbf.core.actions.Get;
 import net.sourceforge.jwbf.core.actions.HttpActionClient;
 import net.sourceforge.jwbf.core.actions.ResponseHandler;
 import net.sourceforge.jwbf.mediawiki.ApiRequestBuilder;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.jgraph.JGraph;
+import org.jgraph.graph.ConnectionSet;
+import org.jgrapht.ListenableGraph;
+import org.jgrapht.ext.JGraphModelAdapter;
+import org.jgrapht.graph.ListenableDirectedGraph;
 
 /**
  *
  * @author orgho
  */
-public class Query {
+public class Query extends JApplet {
 
     static HttpActionClient testee = HttpActionClient.builder()
             .withClient(HttpClientBuilder.create().build())
             .withUrl("https://www.wikipedia.org/w/")
             .build();
+
+    private static final Color DEFAULT_BG_COLOR = Color.decode("#FAFBFF");
+    private static final Dimension DEFAULT_SIZE = new Dimension(4000, 4000);
+
+    @SuppressWarnings("rawtypes")
+    private JGraphModelAdapter jgAdapter;
+
+    @SuppressWarnings({"unchecked", "rawtypes"})
 
     public static String getLinks(String title) {
         title = title.replace(" ", "_");
@@ -50,10 +72,10 @@ public class Query {
         Pattern p = Pattern.compile("(\\*\":\".*?\")", Pattern.DOTALL);
         Matcher m = p.matcher(linksString);
 
-        List<String> links = new ArrayList<String>();
+        List<String> links = new LinkedList<String>();
         while (m.find()) {
             String link = m.group().replace("\"", "").replace("*:", "");
-            if (!(link.contains("Template:") || link.contains("Category:"))) {
+            if (!(link.contains("Template") || link.contains("Category") || links.contains(link))) {
                 links.add(link);
             }
         }
@@ -61,15 +83,53 @@ public class Query {
         return links;
     }
 
+    public void init() {
+        ListenableGraph<String, MyEdge> g = new ListenableDirectedGraph<String, MyEdge>(MyEdge.class);
+        jgAdapter = new JGraphModelAdapter(g);
+        JGraph jgraph = new JGraph(jgAdapter);
+        ConnectionSet cs = new ConnectionSet();
+
+        String article = "Java";
+
+        List<String> links = parseLinks(article);
+
+        g.addVertex(article);
+
+        for (String link : links) {
+            g.addVertex(link);
+            g.addEdge(article, link);
+
+        }
+        jgraph.setAutoResizeGraph(true);
+
+        JGraphLayout layout = new JGraphOrganicLayout(new Rectangle(-500, 0, 700, 700));
+//        JGraphLayout layout = new JGraphSelfOrganizingOrganicLayout();
+        JGraphFacade facade = new JGraphFacade(jgraph);
+        layout.run(facade);
+        Map nested = facade.createNestedMap(false, false);
+
+//        GraphConstants.setLabelEnabled(jgraph, false);
+        jgraph.getGraphLayoutCache().edit(nested);
+        jgraph.validate();
+
+        JScrollPane sp = new JScrollPane(jgraph);
+        getContentPane().add(sp);
+    }
+
     public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException {
+        Query applet = new Query();
+        applet.init();
 
-        List<String> links = parseLinks("Dhaka");
-
+        JFrame frame = new JFrame();
+        frame.getContentPane().add(applet);
+        frame.setTitle("JGraphT Adapter to JGraph Demo");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.show();
 //        UnidrectedGraph<String, DefaultEdge> g
-        PrintWriter out = new PrintWriter("linkDump.JSON", "UTF-8");
-        out.println(links);
-        out.close();
-
+//        PrintWriter out = new PrintWriter("linkDump.JSON", "UTF-8");
+//        out.println(links);
+//        out.close();
     }
 
 }
