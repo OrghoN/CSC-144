11f4fb586b15fa1f5315ce81b5acf6d4d72b6677
Orgho Neogi <anoronyo@gmail.com>
2016-04-01 14:02:13 -0500

    this is the visualization

---
+++ WikipediaPathFinder/src/main/java/wikipediapathfinder/MyEdge.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wikipediapathfinder;
+
+import org.jgrapht.graph.DefaultEdge;
+
+/**
+ *
+ * @author orgho
+ */
+public class MyEdge extends DefaultEdge {
+
+    @Override
+    public String toString() {
+        return "";
+    }
+
+}
