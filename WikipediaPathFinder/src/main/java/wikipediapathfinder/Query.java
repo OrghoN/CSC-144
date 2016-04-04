1ae5ae7437f4cbad863190c008c3d1d696d0bd19
Orgho Neogi <anoronyo@gmail.com>
2016-04-03 23:55:26 -0500

    Added list path and shortest path

--- WikipediaPathFinder/src/main/java/wikipediapathfinder/Query.java
+++ WikipediaPathFinder/src/main/java/wikipediapathfinder/Query.java
@@ -8,7 +8,7 @@ package wikipediapathfinder;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -19,6 +19,7 @@ import net.sourceforge.jwbf.core.actions.ResponseHandler;
 import net.sourceforge.jwbf.mediawiki.ApiRequestBuilder;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.jgrapht.ListenableGraph;
+import org.jgrapht.alg.DijkstraShortestPath;
 import org.jgrapht.graph.DefaultEdge;
 import org.jgrapht.graph.ListenableDirectedGraph;
 
@@ -50,18 +51,23 @@ public class Query {
         return getParse.get().toString();
     }
 
-    public static List<String> parseLinks(String title) {
+    public static List<String> parseLinks(String title, String goal) {
         String linksString = getLinks(title);
         Pattern p = Pattern.compile("(\\*\":\".*?\")", Pattern.DOTALL);
         Matcher m = p.matcher(linksString);
         g.addVertex(title);
 
-        List<String> links = new ArrayList<String>();
+        List<String> links = new LinkedList<String>();
         while (m.find()) {
             String link = m.group().replace("\"", "").replace("*:", "");
-            if (!(link.contains("Template") || link.contains("Category"))) {
+            if (!(link.contains("Template") || link.contains("Category") || g.containsVertex(link))) {
                 g.addVertex(link);
                 g.addEdge(title, link);
+                if (link.equals(goal)) {
+                    List<String> found = new LinkedList<String>();
+                    found.add("Found");
+                    return found;
+                }
                 links.add(link);
             }
         }
@@ -69,14 +75,46 @@ public class Query {
         return links;
     }
 
+    public static boolean parseLinks(List<String> links, String goal) {
+        List<String> found = new LinkedList<String>();
+        found.add("Found");
+
+        List<String> globalLinks = new LinkedList<String>();
+
+        for (String link : links) {
+            List<String> result = parseLinks(link, goal);
+            if (result.equals(found)) {
+                return true;
+            } else {
+                globalLinks.addAll(result);
+            }
+
+        }
+
+        System.out.println(globalLinks);
+        parseLinks(globalLinks, goal);
+//        System.out.println(globalLinks);
+
+        return false;
+
+    }
+
     public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException {
 
-        List<String> links = parseLinks("Mount Vernon, Iowa");
+        String goal = "Achievement test";
+        String start = "Mount Vernon, Iowa";
+        List<String> links = parseLinks(start, goal);
+        boolean result = parseLinks(links, goal);
+
+        DijkstraShortestPath<String, DefaultEdge> pathFinder = new DijkstraShortestPath(g, start, goal);
+        List<DefaultEdge> path = pathFinder.getPathEdgeList();
 
 //        UnidrectedGraph<String, DefaultEdge> g
         PrintWriter out = new PrintWriter("linkDump.JSON", "UTF-8");
-        out.println(g);
-//        System.out.println(g);
+        System.out.println(result);
+        for (Object edge : path) {
+            out.println(edge);
+        }
         out.close();
 
     }
